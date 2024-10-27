fdc1c81f45a581efefc6b3dbffbe9da528b22de6
Tameika Thompson <meekmeek92@MacBook-Pro-82.local>
2024-10-27 19:20:25 -0400

    added gas car class and test ases

--- dist/test/SolarClass.test.js
+++ dist/test/SolarClass.test.js
@@ -1,24 +1,17 @@
-
-
-import { SolarCar } from "../src/SolarClass";
-
-test('should initialize with team name and speed of 0',() => {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { SolarCar } = require("./dist/SolarClass");
+test('should initialize with team name and speed of 0', () => {
     const car = new SolarCar('Team Solar Car Boyz');
     expect(car.team).toBe('Team Solar Car Boyz');
     expect(car.speed).toBe(0);
-})
-
-test('should increment speed by 1 when accelerate is',()=>{
+});
+test('should increment speed by 1 when accelerate is', () => {
     const car = new SolarCar('Team Solar Car Boyz');
     car.accerlerate();
     expect(car.speed).toBe(1);
 });
-
-test('should always return false for isFuelEmpty',()=> {
+test('should always return false for isFuelEmpty', () => {
     const car = new SolarCar('Team Solar Car Boyz');
     expect(car.isFuelEmpty()).toBe(false);
 });
-
-
-
-
